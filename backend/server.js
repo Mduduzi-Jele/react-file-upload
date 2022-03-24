5181d8b8875be456a99fe9f39e6e8deb3d6b0430
Mduduzi-Jele <jelemduduzi@gmail.com>
2022-03-24 13:42:32 +0200

    the first bersion

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var fileUpload = require('express-fileupload');
+
+var app = express();
+
+app.use(fileUpload());
+
+app.post('/upload', (req, res)=> {
+    if(req.files === null){
+        return res.status(400).json({msg: 'No file uploaded'});
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/public/uploads/${file.name}`, err =>{
+        if(err){
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        res.json({fileName: file.name, filePath: `/uploads/${file.name}`});
+    })
+})
+
+app.listen(5000, ()=> console.log('Server Started...'));
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h4 className="display-4 text-center mb-4">React File Upload</h4>
+    <FileUpload />
+    </div>
+  );
+}
+
+export default App;
